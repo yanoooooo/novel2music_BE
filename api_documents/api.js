bb3b07269967777775c9bf1390bd56c98e0be24e
aynishim <aynishim@yahoo-corp.jp>
2016-05-26 18:34:00 +0900

    inital commit

---
+++ api_documents/api.js
@@ -0,0 +1,56 @@
+/**
+ * @swagger
+ * resourcePath: /api
+ * description: All about API
+ */
+ 
+/**
+ * @swagger
+ * path: /login
+ * operations:
+ *   -  httpMethod: POST
+ *      summary: Login with username and password
+ *      notes: Returns a user based on username
+ *      responseClass: User
+ *      nickname: login
+ *      consumes:
+ *        - text/html
+ *      parameters:
+ *        - name: username
+ *          description: Your username
+ *          paramType: query
+ *          required: true
+ *          dataType: string
+ *        - name: age
+ *          description: Your age
+ *          paramType: query
+ *          required: true
+ *          dataType: int
+ *        - name: password
+ *          description: Your password
+ *          paramType: query
+ *          required: true
+ *          dataType: string
+ */
+
+exports.login = function (req, res) {
+  var user = {};
+  user.username = req.param('username');
+  user.age = req.param('age');
+  user.password = req.param('password');
+  res.json(user);
+}
+
+/**
+ * @swagger
+ * models:
+ *   User:
+ *     id: User
+ *     properties:
+ *       username:
+ *         type: String
+ *       age:
+ *         type: Int
+ *       password:
+ *         type: String
+ */
