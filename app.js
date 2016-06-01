7e368bf23f27180e10b9a2c6c3ebf6fadad2ae1c
aynishim <aynishim@yahoo-corp.jp>
2016-06-01 10:08:59 +0900

    headerの追加

--- app.js
+++ app.js
@@ -21,6 +21,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.use(express.static(path.join(__dirname, 'public')));
 
 //ここでv1とかルーティングをし、ファイルを指定する
