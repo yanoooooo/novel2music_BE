f029c2898a18fe41dcb588851679f52dbc249a1c
yanoooooo <miserablescaromioben@gmail.com>
2016-06-05 14:13:00 +0900

    GET scale all の追加

---
+++ routes/v1/scale.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var Sqlite = require('../sqlite.js');
+var sqlite = new Sqlite();
+
+var router = express.Router();
+router.use(bodyParser());
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    var sql = "SELECT id, name FROM master_scale";
+    sqlite.select(sql).then(function(result) {
+        if(result.status === true) {
+            res.status(200).json(result);
+        } else {
+            res.status(400).json(result);
+        }
+    });
+});
+
+module.exports = router;
